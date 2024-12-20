f4457ca23f10177923a48d886f62995d0b91ea82
Swapvp <swapneshpawar1994@gmail.com>
2024-12-20 16:32:28 +0530

    stepper-calculator

---
+++ Calculator/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
+
+// https://vite.dev/config/
+module.exports = defineConfig({
+  plugins: [react()],
+});
